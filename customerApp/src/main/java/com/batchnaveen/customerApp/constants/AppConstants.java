bca5b8f7a674e5240c7e618a9857f6a114430e9d
Naveen Kumar <naveenkumar16613@gmail.com>
2025-05-30 14:11:22 +0530

    first commit

---
+++ customerApp/src/main/java/com/batchnaveen/customerApp/constants/AppConstants.java
@@ -0,0 +1,7 @@
+package com.batchnaveen.customerApp.constants;
+
+public class AppConstants {
+	public static final String TOPIC = "order_topic";
+	public static final String KAFKA_HOST = "localhost:9092";
+
+}
